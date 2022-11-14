b604d81fb7d59f17526e81a58e31e27c42cca2e3
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2022-11-14 15:59:16 -0500

    FIX: restored count reactivity in BuyOrdersList

--- src/pages/BuyOrderList.tsx
+++ src/pages/BuyOrderList.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import { ActionBox, Box, Flex, Grid, H3 } from "../elements/basics";
 import { Clickable } from "../elements/Clickable";
 import {
-  GlobalCountrySelectionSummary,
-  GlobalCountrySelector,
-  useGlobalCountyList,
+GlobalCountrySelectionSummary,
+GlobalCountrySelector,
+useGlobalCountyList,
 } from "../elements/GlobalCountrySelection";
 import { Loading } from "../elements/Loading";
 import { Overlay } from "../elements/Overlay";
@@ -16,86 +16,87 @@ import { datasets } from "../utils/apiSamples";
 import { forcastedRecordCount } from "../utils/logic";
 
 function BuyOrderField({ name, value }: { name: string; value: string }) {
-  return (
-    <Flex px={2} py={3} backgroundColor="tone3" flexDirection="column">
-      <H3>{name}</H3>
-      <Box>{value}</Box>
-    </Flex>
-  );
+return (
+  <Flex px={2} py={3} backgroundColor="tone3" flexDirection="column">
+    <H3>{name}</H3>
+    <Box>{value}</Box>
+  </Flex>
+);
 }
 
 function buyOrderItem(
-  countries: Country[],
-  datasets: Dataset[],
-  buyOrder: BuyOrder
+countries: Country[],
+datasets: Dataset[],
+buyOrder: BuyOrder
 ) {
-  const forcasted = forcastedRecordCount(countries, datasets, buyOrder);
-  return [
-    <div key={`${buyOrder.id}-name`}>
-      <Link to={`/view-buy-order/${buyOrder.id}`}>
-        <BuyOrderField name="Order name" value={buyOrder.name} />
-      </Link>
-    </div>,
-    <div key={`${buyOrder.id}-createdAt`}>
-      <Link to={`/view-buy-order/${buyOrder.id}`}>
-        <BuyOrderField
-          name="Date Created"
-          value={new Date(buyOrder.createdAt).toLocaleString([], {
-            year: "numeric",
-            month: "numeric",
-            day: "numeric",
-          })}
-        />
-      </Link>
-    </div>,
-    <div key={`${buyOrder.id}-forcasted`}>
-      <Link to={`/view-buy-order/${buyOrder.id}`}>
-        <BuyOrderField
-          name="Forecasted Records"
-          value={
-            forcasted.toLocaleString() +
-            " " +
-            (forcasted === 1 ? "record" : "records")
-          }
-        />
-      </Link>
-    </div>,
-  ];
+const forcasted = forcastedRecordCount(countries, datasets, buyOrder);
+return [
+  <div key={`${buyOrder.id}-name`}>
+    <Link to={`/view-buy-order/${buyOrder.id}`}>
+      <BuyOrderField name="Order name" value={buyOrder.name} />
+    </Link>
+  </div>,
+  <div key={`${buyOrder.id}-createdAt`}>
+    <Link to={`/view-buy-order/${buyOrder.id}`}>
+      <BuyOrderField
+        name="Date Created"
+        value={new Date(buyOrder.createdAt).toLocaleString([], {
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+        })}
+      />
+    </Link>
+  </div>,
+  <div key={`${buyOrder.id}-forcasted`}>
+    <Link to={`/view-buy-order/${buyOrder.id}`}>
+      <BuyOrderField
+        name="Forecasted Records"
+        value={
+          forcasted.toLocaleString() +
+          " " +
+          (forcasted === 1 ? "record" : "records")
+        }
+      />
+    </Link>
+  </div>,
+];
 }
 
 function BuyOrderList({
-  buyOrders,
-  countries,
-  datasets,
-  ...props
+buyOrders,
+countries,
+datasets,
+...props
 }: {
-  buyOrders: BuyOrder[];
-  countries: Country[];
-  datasets: Dataset[];
-  [prop: string]: unknown; // TODO: could be more restrictive
+buyOrders: BuyOrder[];
+countries: Country[];
+datasets: Dataset[];
+[prop: string]: unknown; // TODO: could be more restrictive
 }) {
-  const [activeCountryCode] = useGlobalCountyList(countries);
-  return (
-    <Grid gridTemplateColumns={"auto auto auto"} gridRowGap="3" {...props}>
-      {buyOrders
-        .filter((buyOrder) =>
-          buyOrder.countries.some((countryCode) =>
-            activeCountryCode.includes(countryCode)
-          )
-        )
-        .flatMap((buyOrder) => buyOrderItem(countries, datasets, buyOrder))}
-    </Grid>
-  );
+return (
+  <Grid gridTemplateColumns={"auto auto auto"} gridRowGap="3" {...props}>
+    {buyOrders.flatMap((buyOrder) =>
+      buyOrderItem(countries, datasets, buyOrder)
+    )}
+  </Grid>
+);
 }
 
 function Resolved({
-  buyOrders,
-  countries,
+buyOrders,
+countries,
 }: {
-  buyOrders: BuyOrder[];
-  countries: Country[];
+buyOrders: BuyOrder[];
+countries: Country[];
 }) {
-  const [overlayVisible, setOverlayvisible] = useState(false);
+const [overlayVisible, setOverlayvisible] = useState(false);
+const [activeCountryCode] = useGlobalCountyList(countries);
+  const relevantBuyOrders = buyOrders.filter((buyOrder) =>
+    buyOrder.countries.some((countryCode) =>
+      activeCountryCode.includes(countryCode)
+    )
+  );
   return (
     <>
       <Overlay
@@ -108,14 +109,14 @@ function Resolved({
         <Flex flexDirection="column" width={6}>
           <Clickable onClick={() => setOverlayvisible(true)}>
             <GlobalCountrySelectionSummary
-              count={buyOrders.length}
+              count={relevantBuyOrders.length}
               countries={countries}
             />
           </Clickable>
           <BuyOrderList
             countries={countries}
             datasets={datasets}
-            buyOrders={buyOrders}
+            buyOrders={relevantBuyOrders}
           />
         </Flex>
       </Flex>
